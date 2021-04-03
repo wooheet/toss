cdd906914e87191adcc8004bc4cc1732e95b8b35
gary <gary@spoonradio.co>
2021-04-03 16:29:55 +0900

    Added auth user model to settings

--- users/views.py
+++ users/views.py
@@ -3,7 +3,7 @@ from rest_framework import viewsets, mixins, views
 from rest_framework.decorators import action
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated, AllowAny
-from django.contrib.auth.models import User
+from .models import User
 from .serializers import UserSerializer, MyProfileSerializer
 from config.mixins import CustomResponseMixin, CustomPaginatorMixin
 
@@ -13,29 +13,30 @@ class UserViewSet(viewsets.ModelViewSet,
                   CustomPaginatorMixin):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = (AllowAny,)
 
-    # @action(detail=False, methods=['get'])
-    # def me(self, request, *args, **kwargs):
-    #     """
-    #         본인 Profile 조회 API
-    #     ---
-    #     response_serializer: users.serializers.MyProfileSerializer
-    #     parameters:
-    #         -   name: pk
-    #             description : Profile 정보를 조회 할 User ID
-    #             paramType: path
-    #     responseMessages:
-    #         -   code:   200
-    #             message: SUCCESS
-    #         -   code:   404
-    #             message: NOT FOUND
-    #         -   code:   500
-    #             message: SERVER ERROR
-    #     """
-    #     if not request.user.is_authenticated:
-    #         return self.un_authorized()
-    #
-    #     serializer = MyProfileSerializer(
-    #         request.user, context=dict(request=request)
-    #     )
-    #     return self.success(results=serializer.data)
+    @action(detail=False, methods=['get'])
+    def me(self, request, *args, **kwargs):
+        """
+            본인 Profile 조회 API
+        ---
+        response_serializer: users.serializers.MyProfileSerializer
+        parameters:
+            -   name: pk
+                description : Profile 정보를 조회 할 User ID
+                paramType: path
+        responseMessages:
+            -   code:   200
+                message: SUCCESS
+            -   code:   404
+                message: NOT FOUND
+            -   code:   500
+                message: SERVER ERROR
+        """
+        if not request.user.is_authenticated:
+            return self.un_authorized()
+
+        serializer = MyProfileSerializer(
+            request.user, context=dict(request=request)
+        )
+        return self.success(results=serializer.data)
