f5e5226c7b63d110e8242677a81c97655fa6fde5
haewon <30405705+wooheet@users.noreply.github.com>
2021-04-07 23:40:32 +0900

    Edit user url

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path
 from django.conf.urls import include
 from rest_framework import routers
-from .views import UserViewSet, TokenViewSet
+from .views import UserViewSet
 
 router = routers.DefaultRouter()
 router.register(r'', UserViewSet, basename='user')
-router.register(r'token', TokenViewSet, basename='token')
-router.register(r'token/refresh', TokenViewSet, basename='token-refresh')
 
 urlpatterns = [
     path('', include(router.urls)),
