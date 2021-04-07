898fe93bfd788da7cec6ffb2b83ab45e92db1aba
haewon <30405705+wooheet@users.noreply.github.com>
2021-04-07 23:54:20 +0900

    Edited core model

--- core/models.py
+++ core/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class TimeStampedModel(models.Model):
     """ Time Stamped Model """
 
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         abstract = True
